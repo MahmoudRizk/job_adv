60a35b9d1ca7c4f29fe216c1e50261aa620e8dbc
mahmoudrizk <mahmoudahmedrizk@gmail.com>
2020-07-15 15:26:27 +0200

    Add account table view and CV download route

--- account/router.py
+++ account/router.py
@@ -2,14 +2,16 @@ from ..app import app
 from .model import Account
 from .view import render_table_view, render_form_view
 
-from flask import request
+from flask import request, send_file
 from werkzeug.utils import secure_filename
 
 import os
 
 @app.route('/account')
 def account():
-    return render_table_view()
+    query = Account.query.all()
+
+    return render_table_view(query)
 
 @app.route('/account/create', methods = ['GET', 'POST'])
 def account_create():
@@ -28,9 +30,9 @@ def account_create():
 
         file = request.files['cv'] if 'cv' in request.files else None
         if file:
-            filename = user_name
-            cv_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(cv_path)
+            filename = email_address
+            cv_path = '/account/cv/%s' % filename
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
         account = Account(user_name, email_address, password, is_employee, is_employer, is_admin, cv_path, birth_date)
         account.create()
@@ -39,3 +41,7 @@ def account_create():
 
     elif request.method == 'GET':
         return render_form_view()
+
+@app.route('/account/cv/<name>', methods=['GET'])
+def account_cv(name):
+    return send_file(os.path.join(app.config['UPLOAD_FOLDER'], name), as_attachment=True)
--- account/view.py
+++ account/view.py
@@ -1,8 +1,8 @@
 from flask import render_template
 
 
-def render_table_view():
-    return render_template('account_table_view.html', test_var="xxxx")
+def render_table_view(rows):
+    return render_template('account_table_view.html', rows=rows)
 
 def render_form_view():
     return render_template('account_form_view.html')
